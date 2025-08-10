32e687480733e532f658e88ae30706568e3c2cc2
QoraFi <qoradefi@gmail.com>
2025-08-10 15:33:06 -0600

    Update fetchFlows.js

--- utils/fetchFlows.js
+++ utils/fetchFlows.js
@@ -1,1 +1,9 @@
+export default async function fetchFlows() {
+  // Placeholder: Replace with Dune, Glassnode, or other inflow/outflow API
+  // Example static numbers for now
+  return {
+    netInflow: 30000000,  // USD value of stablecoins entering exchanges
+    netOutflow: 10000000  // USD value leaving exchanges
+  };
+}
 
